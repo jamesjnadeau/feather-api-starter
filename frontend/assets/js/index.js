b508659f4cb0ea22a78cc2780d7eec351a8f6824
jamesjnadeau <james.nadeau@gmail.com>
2015-12-18 19:56:22 -0500

    moving to global serverGetter, fixing mongo discconnect in dev-sever

--- frontend/assets/js/index.js
+++ frontend/assets/js/index.js
@@ -1,27 +1,15 @@
 console.log('Start.');
 require('bootstrap/dist/js/bootstrap.js');
 var notify = require('notifyUtil');
-
-var service = require('_local/assets/js/utils/serviceGetter')(window.API_URL);
+var url = window.API_URL;
+console.log(url)
+var service = require('_local/assets/js/utils/serviceGetter')(url);
 
 var testService = service('content');
 
-/* test create
-testService.on('created', function(todo) {
-  console.log('content created', todo);
+testService.count({}, function(count) {
+  notify('Content Count', JSON.stringify(count), 'success');
+  console.log('test 4');
 });
 
-testService.create({
-  type: 'news',
-  fields: { test: true }
-}).then(function(todo) {
-  console.log('Success!');
-});
-*/
-
-testService.count(function(count) {
-  console.log(error, values);
-  values.forEach(function(value) {
-    notify('Content Count', JSON.stringify(count), 'success');
-  })
-});
+console.log('End');
